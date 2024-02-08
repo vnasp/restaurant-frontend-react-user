49806f5da939dc5dfeff51aa05ebcb4622cbabb0
Valentina Muñoz <valentinamr@gmail.com>
2024-02-08 18:18:37 -0300

    :rocket: Preparing deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-  base: import.meta.env.VITE_BASE_URL,
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [react()]
 });
